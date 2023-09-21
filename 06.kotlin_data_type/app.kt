afc14b2e4891c617dcea419110f4c27722001a65
rejaul <devrejaul.official@gmail.com>
2023-09-22 01:02:27 +0600

    add

---
+++ 06.kotlin_data_type/app.kt
@@ -0,0 +1,40 @@
+// kotlin automatic ditac the data type like jsavascript
+// example 
+val mynumber = 20 // this is number data type :Int
+val myDoubleNumber = 12.45 // this Double number data type 
+val myLeatter = "D" // Car data type 
+val myBoolean = true // Boolean data type 
+val myText = "hello world" // this String data type 
+
+
+// we are also define data type manualy
+// examle 
+val mynumber2: Int = 20 
+val myDoubleNumber2: Double =  20.5  
+val myLeatter2: Char  = 'D'
+val myBoolean2: Boolean = false
+val myText2: String = "hello world"
+
+// define array data using arrayOf() method
+val cars = arrayOf("Volvo", "BMW", "Ford", "Mazda")
+
+fun main() {
+    println(cars)
+}
+
+
+// note all of the numaric data type 
+// Integer types
+//01. Byte, 
+//02. Short, 
+//03. Int
+//04. Long
+
+// Floating point types 
+// 01. Float 
+//02. Double
+
+
+
+// type covert numaric data most use method 
+// toByte(), toShort(), toInt(), toLong(), toFloat(), toDouble() or toChar():
---
+++ 07.kotlin_Operetors/app.kt
@@ -0,0 +1,19 @@
+// Arithmetic operators
+
+fun main() {  
+    // 01 	Addition oprators +
+    var x = 5
+    var y = 3
+    println(x + y)  
+    
+    // Subtraction -
+    var number1 = 5
+    var number2 = 3
+    println(number1 - number2)  
+
+    // 	Multiplication *
+    var mulNumber1 = 2 
+    var mulNumber2 = 5
+    println(mulNumber1*mulNumber2)
+
+  }
